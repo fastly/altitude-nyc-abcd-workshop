5db84075b3566b1965827f48a38eee746acc3852
phamann <patrick@fastly.com>
2017-03-19 12:35:52 -0400

    Add Terraform and Travis template files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,2 +1,37 @@
 # Add your configuration below
+variable "fastly_api_token" {
+  type = "string"
+}
 
+provider "fastly" {
+  api_key = "${var.fastly_api_token}"
+}
+
+resource "fastly_service_v1" "service" {
+  name = "< REPLACE WITH YOUR SERVICE NAME >"
+
+  force_destroy = true
+
+  domain {
+    name    = "<REPLACE WITH YOUR SERVICE NAME >.fastly-altitude-2017.com"
+    comment = "Demo domain"
+  }
+
+  backend {
+    address               = "storage.googleapis.com"
+    ssl_hostname          = "altitude-nyc-abcd-2017-stage.storage.googleapis.com"
+    name                  = "altitude-nyc-abcd-2017-stage"
+    port                  = 443
+    first_byte_timeout    = 3000
+    max_conn              = 200
+    between_bytes_timeout = 1000
+  }
+
+  header {
+    name        = "backend-host-override"
+    action      = "set"
+    type        = "request"
+    destination = "http.Host"
+    source      = "\"altitude-nyc-abcd-2017-stage.storage.googleapis.com\""
+  }
+}
